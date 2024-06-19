5d4f4c6ccf404ba09791eb366e59c875e4164337
Itodo Simon <itodosimonitodo1@gmail.com>
2024-06-19 14:30:47 +0100

    added app name to the meta

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ const generateRandomPassword = require("./passwordGenerator");
 
 function App() {
   const password = generateRandomPassword(10, true, true, true, true);
-  // console.log(password);
+  console.log(password);
 
   return (
     <div className="app">
